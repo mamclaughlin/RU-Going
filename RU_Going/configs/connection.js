8f16826b4b8882e6ef969ab7867a729014d49860
peter tumulty <ptumulty923@gmail.com>
2016-07-21 20:12:46 -0400

    set up backend

--- RU_Going/configs/connection.js
+++ RU_Going/configs/connection.js
@@ -1,40 +1,44 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
 
-//
-// const Sequelize = require("sequelize"),
-// 	  logger    = require("winston");
-//
-// var db = new Sequelize("beallies", "beallies", "Iron_1_Cloud", {
-//
-// 	host: "beallies-1.cluster-cnxvffnypwe9.us-east-1.rds.amazonaws.com",
-// 	port: 3306,
-// 	dialect: "mysql",
-// 	timezone: "America/New_York",
-//
-// 	define: {
-// 		timestamps: false // true by default. these can be set on every sequelize .define
-// 	}
-// 	// host: "exampleconnection.mydbhost.com",
-// 	// port: 3306,
-// 	// dialect: "mysql",
-// 	// timezone: "America/New_York",
-//     //
-// 	// define: {
-// 	// 	timestamps: false // true by default. these can be set on every sequelize .define
-// 	// }
-//
-// });
-//
-// db.authenticate().then(function (err) {
-// 	if (err) {
-// 		logger.error("SEQUELIZE CONNECTION ERROR - Please check your connection!");
-// 	} else {
-// 		logger.info("MySQL Connection Successful!");
-// 	}
-// });
-//
-// module.exports = {
-//
-// 	Sequelize,
-// 	db
-//
-// };
+// Dependencies
+var Sequelize = require("sequelize");
+
+// Lists out connection options
+var source = {
+
+    localhost: {
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: "",
+        database: "rumeeting"
+    },
+
+    jawsDB: {
+        port: 3306,
+        host: 'nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'v3g64qrygw6fmf68',
+        password: "jxv09vrb8gza1pg6",
+        database: "o1ir6nmc2vf9vg3w" 
+    }
+}
+
+// Selects a connection (can be changed quickly as needed)
+var selectedSource = source.jawsDB;
+
+// Creates mySQL connection using Sequelize
+var sequelize = new Sequelize(selectedSource.database, selectedSource.user, selectedSource.password, {
+  host: selectedSource.host,
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+});
+
+// Exports the connection for other files to use
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Requirements
 const express      = require("express"),
       app          = express(),
 	  PORT         = process.env.PORT || 3000,
@@ -6,41 +7,36 @@ const express      = require("express"),
 	  bodyParser   = require("body-parser"),
 	  cookieParser = require("cookie-parser"),
 	  path         = require("path"),
-	  logger       = require("morgan")
-
-const mysql        = require("mysql")
-      //need MYSQL db
-	  // db           = require("./beallies/models/model-index.js"); // index file that controls sequelize models
-
+	  logger       = require("morgan");
+const mysql        = require("mysql"); 
 const mongo = require("mongodb"),
 	  mongoose = require("mongoose");
-
 const moment       = require("moment"),
 	  now          = moment().format();
 
+//Setting up parsing to the client side 
 app.use(logger('dev'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); // choose whether to use "querystring" or "qs" parsing library
 app.use(cookieParser("secret"));
-
-
 app.use(express.static(__dirname + "RU_Going/public")); // default static folder
-app.use("/node_modules", express.static(__dirname + "/node_modules")); // define static route for node modules, for a <script> link to angular for example
-// app.use(express.static('public'));
+app.use("/node_modules", express.static(__dirname + "/node_modules"));
 
 
-// //Mongoose Database configuration -- CHANGE THIS TO WHATEVER WE USE
-// mongoose.connect('mongodb://mariah:nodeuser@ds049925.mlab.com:49925/heroku_1rm9fzqq');
-// var db = mongoose.connection;
-//
-// db.on('error', function (err) {
-// 	console.log('Mongoose Error: ', err);
-// });
-// db.once('open', function () {
-// 	console.log('Mongoose connection successful.');
-// });
+//Connecting to MongoDB
+mongoose.connect('mongodb://rumeeting1:rumeetingrumeeting@ds027165.mlab.com:27165/meetru');
 
+//Testing connection to MongoDB
+var dbm = mongoose.connection;
+dbm.on('error', function (err) {
+	console.log('Mongoose Error: ', err);
+});
+dbm.once('open', function () {
+	console.log('Mongoose connection successful.');
+});
+
+//Getting MySQL Connection
+var models = require('./RU_Going/configs/connection.js');
 
 
 // Routes
@@ -48,6 +44,8 @@ app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/RU_Going/public/index.html'));
 	// res.send(index.html);
 });
+
+
 
 //Start server
 app.listen(PORT, function() {

48b3d8aa8909c25a1282c0dbc0fcd4ff1218aef5
Mariah McLaughlin <hello@mariahmclaughlin.com>
2016-07-22 18:06:24 -0400

    made new landing page. put old index into school.html

---
+++ RU_Going/configs/auth.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Mariah on 7/22/16.
+ */
+
+module.exports = {
+	'facebookAuth' : {
+		'clientID' : '258318227857693',
+		'clientSecret' : '2971b6544cf1f17127c293f99fa30533',
+		'callbackURL' : 'https://localhost:3000/auth/facebook/callback'
+	}
+}
--- RU_Going/configs/passport.js
+++ RU_Going/configs/passport.js
@@ -1,86 +1,106 @@
+var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
-// const LocalStrategy = require("passport-local").Strategy;
-// const Sequelize     = require("../configs/dbconnection"),
-// 	  model         = require("../models/model-index");
-//
-// const winston  = require("winston"),
-//
-// 	logger = new winston.Logger({
-// 		level: "debug", // will log this and every level below it
-// 		transports: [
-// 			new (winston.transports.Console)(),
-// 			new (winston.transports.File)({ filename: "./logs/log-test.log"})
-// 			// AWS cloudwatch transport will go here
-// 		]
-// 	});
-//
-// module.exports = function(passport) {
-//
-// 	// serialize the user for the session
-// 	passport.serializeUser(function(user, done) {
-// 		done(null, user.id);
-// 	});
-//
-// 	// deserialize user
-// 	passport.deserializeUser(function(id, done) {
-// 		model.User.findById(id, function(err, user) {
-// 			done(err, user);
-// 		})
-// 	});
-//
-// 	// TODO: change this to use promises -kevh 5/31 @ 7:30 PM
-// 	// TODO: check if correct info is being serialized into the user session
-// 	// TODO: add further validation to user signup input
-// 	// local auth strategy. using named strats instead of the default "local"
-// 	passport.use("local-signup", new LocalStrategy({
-// 		usernameField: "userName", // override username field with email
-// 		passwordField: "password",
-// 		passReqToCallback: true // pass entire req to callback
-// 	},
-// 		function(req, userName, password, done) { // since passReqToCallback is TRUE, we can access the req.body for more data
-//
-// 			process.nextTick(function() {
-//
-// 				model.User.find({
-//
-// 					where: Sequelize.Sequelize.or({ userName: userName}, { userEmail: req.body.email})
-//
-// 				}).then(function (user){
-//
-// 					// IF no user exists, create new user
-// 					if (!user) {
-// 						model.User.create({
-//
-// 							userEmail: req.body.email,
-// 							userName : userName,
-// 							userPass : password });
-//
-// 						logger.info("User " + userName +  " created successfully!");
-// 						return done(null, user)
-// 					}
-//
-// 					if (user) {
-// 						// IF userEmail already exists
-// 						if (user.userEmail === req.body.email) {
-// 							logger.error("ERROR: email already exists for " + req.body.email);
-// 							return done(null, false, req.flash("signupMessage", "Email already exists"));
-// 						}
-//
-// 						// IF userName already exists
-// 						if (user.userName === userName) {
-// 							logger.error("ERROR: Username already exists for " + userName);
-// 							return done(null, false, req.flash("signupMessage", "Username already exists"));
-// 						}
-// 					}
-//
-// 				}).catch(function (err) {
-// 					logger.error(err);
-// 				});
-//
-// 			});
-//
-// 		}
-//
-// 	));
-//
-// };
+
+var User       = require('../models/user.js');
+var configAuth = require('../config/auth');
+
+module.exports = function(passport) {
+
+
+	passport.serializeUser(function(user, done){
+		done(null, user.id);
+	});
+
+	passport.deserializeUser(function(id, done){
+		User.findById(id, function(err, user){
+			done(err, user);
+		});
+	});
+
+
+	passport.use('local-signup', new LocalStrategy({
+			usernameField: 'email',
+			passwordField: 'password',
+			passReqToCallback: true
+		},
+		function(req, email, password, done){
+			process.nextTick(function(){
+				User.findOne({'local.username': email}, function(err, user){
+					if(err)
+						return done(err);
+					if(user){
+						return done(null, false, req.flash('signupMessage', 'That email already taken'));
+					} else {
+						var newUser = new User();
+						newUser.local.username = email;
+						newUser.local.password = newUser.generateHash(password);
+
+						newUser.save(function(err){
+							if(err)
+								throw err;
+							return done(null, newUser);
+						})
+					}
+				})
+
+			});
+		}));
+
+	passport.use('local-login', new LocalStrategy({
+			usernameField: 'email',
+			passwordField: 'password',
+			passReqToCallback: true
+		},
+		function(req, email, password, done){
+			process.nextTick(function(){
+				User.findOne({ 'local.username': email}, function(err, user){
+					if(err)
+						return done(err);
+					if(!user)
+						return done(null, false, req.flash('loginMessage', 'No User found'));
+					if(!user.validPassword(password)){
+						return done(null, false, req.flash('loginMessage', 'invalid password'));
+					}
+					return done(null, user);
+
+				});
+			});
+		}
+	));
+
+
+	passport.use(new FacebookStrategy({
+			clientID: configAuth.facebookAuth.clientID,
+			clientSecret: configAuth.facebookAuth.clientSecret,
+			callbackURL: configAuth.facebookAuth.callbackURL
+		},
+		function(accessToken, refreshToken, profile, done) {
+			process.nextTick(function(){
+				User.findOne({'facebook.id': profile.id}, function(err, user){
+					if(err)
+						return done(err);
+					if(user)
+						return done(null, user);
+					else {
+						var newUser = new User();
+						newUser.facebook.id = profile.id;
+						newUser.facebook.token = accessToken;
+						newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
+						newUser.facebook.email = profile.emails[0].value;
+
+						newUser.save(function(err){
+							if(err)
+								throw err;
+							return done(null, newUser);
+						})
+						console.log(profile);
+					}
+				});
+			});
+		}
+
+	));
+
+
+};
